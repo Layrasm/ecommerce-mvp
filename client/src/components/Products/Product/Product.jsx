8a3d31c4b56ceae31d14f81b44a12ced6cf9f817
Layne Rasmussen <layrasm@gmail.com>
2021-05-09 16:58:17 -0600

    removed material UI styling file, just created product.css stylesheet

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-import useStyles from './styles';
+import "./product.css";
 
 const Product = ({ product }) => {
-  const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardMedia className={classes.media} image='' title={product.name} />
-      <CardContent>
-        <div className={classes.cardContent}>
+    <Card className="root">
+      < CardMedia className="media" image='' title={product.name} />
+      <CardContent CardContent >
+        <div className="cardContent">
           <Typography variant="h5" gutterBottom>
             {product.name}
           </Typography>
           <Typography variant="h5">
             {product.price}
           </Typography>
-          <Typography variant="h2" color="textSecondary">
-            {product.description}
-          </Typography>
-
         </div>
-      </CardContent>
-      <CardActions disableSpacing className={classes.cardActions}>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+
+
+      </CardContent >
+      <CardActions disableSpacing className="cardActions">
         <IconButton aria-label="Add To Cart">
           <AddShoppingCart />
         </IconButton>
       </CardActions>
-    </Card>
+    </Card >
   )
 }
 
