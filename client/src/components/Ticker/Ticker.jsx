ceae4be69c91fa395b8b2219d90856855e57593a
Layne Rasmussen <layrasm@gmail.com>
2021-05-18 14:19:25 -0600

    general formatting for ticker component complete

--- client/src/components/Ticker/Ticker.jsx
+++ client/src/components/Ticker/Ticker.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './ticker.css';
+import logo from '../../assets/Dogecoin_Logo.png';
+import PriceCard from './PriceCard';
+
 
 const Ticker = () => {
   return (
     <div className="tickerMain">
-      <h1>Ticker 3894y9hdhfjldfshgdfhsgp9h4890h59</h1>
+      <img src={logo} width={150} height={150} alt="dogecoin logo" />
+      <h1 className="titleTicker">Live Dogecoin Price</h1>
+      <h5 className="subtitleTicker">To The Moon 🚀🌕</h5>
+      <div className="pricesContainer">
+        <PriceCard type="low" price={100} />
+        <PriceCard type="high" price={200} />
+        <PriceCard type="current" price={150} />
+      </div>
     </div>
   )
 }
